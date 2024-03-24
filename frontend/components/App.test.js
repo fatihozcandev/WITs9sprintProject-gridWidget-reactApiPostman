ad2f5a651491ae7c23ace603c1934365e08c35c6
fatihozcandev <fatih-ozcan@windowslive.com>
2024-03-25 00:57:55 +0100

    final

---
+++ frontend/components/App.test.js
@@ -0,0 +1,31 @@
+import { render } from "@testing-library/react";
+import AppFunctional from "./AppFunctional";
+import React from "react";
+
+test("Render oluyor mu?", () => {
+  render(<AppFunctional />);
+});
+test("Render oluyor mu?", () => {
+  render(<AppFunctional />);
+});
+test("Render oluyor mu?", () => {
+  render(<AppFunctional />);
+});
+test("Render oluyor mu?", () => {
+  render(<AppFunctional />);
+});
+test("Render oluyor mu?", () => {
+  render(<AppFunctional />);
+});
+test("Render oluyor mu?", () => {
+  render(<AppFunctional />);
+});
+test("Render oluyor mu?", () => {
+  render(<AppFunctional />);
+});
+test("Render oluyor mu?", () => {
+  render(<AppFunctional />);
+});
+test("Render oluyor mu?", () => {
+  render(<AppFunctional />);
+});
